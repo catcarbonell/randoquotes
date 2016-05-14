1db835a32ea2bc016c7aaf9595d45c16411cc095
Cathleen Carbonell <catcarbonell@gmail.com>
2016-05-14 13:59:07 -0700

    Formatting

--- js/main.js
+++ js/main.js
@@ -1,6 +1,47 @@
 // Treehouse Random Quote Generator
 // Author: Cat Carbonell (@catcarbonell)
 
+// event listener to respond to clicks on the page
+// when user clicks anywhere on the page, the "makeQuote" function is called
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
 // Generate random quotes
+
+// Global variable for quotes
+var message = '';
+
+ 
+// Find the element that will display the quote
+ 
+function print(message) {     
+        var output = document.getElementById('quote-container');
+        output.innerHTML = message;
+}   
+
+// Get the random quote to be random
+function getRandomQuote() {
+        var quoteObject = quotes[Math.floor(Math.random() * quotes.length)];
+        return quoteObject;
+}
+
+// "Physically" print the quote, source, and optional keys into the div
+function printQuote() {
+		var printObject = getRandomQuote();
+        message += '<h1 class="quotes">' + printObject.quote + '</h1>';
+        message += '<h3 class="source">' + printObject.source + '</h2>';
+        if (printObject.citation !== undefined) {
+                message += '<span class ="citation">' + printObject.citation + '</span>';
+            }
+        if (printObject.year !== undefined) {
+                message += '<span class ="year"> ' + printObject.year + '</span>';
+            }
+        message += '</p>';
+        print(message);
+        message = '';
+        //var getRandomColors = RandomColors();
+	    //document.body.style.backgroundColor = getRandomColors;
+}
 
 // Generate random background color
--- js/quotes.js
+++ js/quotes.js
@@ -4,6 +4,8 @@
 // List of various quotes 
 
 var quotes = [
+
+    //Obligatory Bruce Lee quote
     { 
         quote: 'Absorb what is useful, Discard what is not, Add what is uniquely your own.',
         source: 'Bruce Lee',
@@ -11,51 +13,43 @@ var quotes = [
     },
 
     { 
-        quote: '',
-        source: '',
-        citation: '',
-        year: '',
-        tags: ''
+        quote: 'At the touch of love, everyone becomes a poet.',
+        source: 'Plato',
+        tags: 'love'
     },
 
     { 
-        quote: '',
-        source: '',
-        citation: '',
-        year: '',
-        tags: ''
+        quote: 'You know you\'re in love when you don\'t want to fall asleep because reality is finally better than your dreams.',
+        source: 'Dr. Seuss',
+        tags: 'love'
     },
 
     { 
-        quote: '',
-        source: '',
-        citation: '',
-        year: '',
-        tags: ''
+        quote: 'Nothing diminishes anxiety faster than action.',
+        source: 'Walter Anderson',
+        citation: 'The Confidence Course',
+        year: '1997',
+        tags: 'motivational'
     },
 
     { 
-        quote: '',
-        source: '',
-        citation: '',
-        year: '',
-        tags: ''
+        quote: 'Many a true word is spoken in jest.',
+        source: 'English Proverb',
+        tags: 'humor'
     },
 
     { 
-        quote: '',
-        source: '',
-        citation: '',
-        year: '',
-        tags: ''
+        quote: 'Listen, Morty, I hate to break it to you but what people call love is just a chemical reaction that compels animals to breed. It hits hard, Morty, then it slowly fades, leaving you stranded in a failing marriage. I did it. Your parents are gonna do it. Break the cycle, Morty. Rise above. Focus on science.',
+        source: 'Rick Sanchez',
+        citation: 'Rick and Morty',
+        tags: 'love'
     },
     
     { 
-        quote: '',
-        source: '',
-        citation: '',
-        year: '',
-        tags: ''
+        quote: 'There is no rest for the weary and wounded until the job is done',
+        source: 'Peggy Carter',
+        citation: 'Marvel\'s Agent Carter',
+        tags: 'motivational'
     }
 
-]
+];
